41a77e2c7c2ac95ca63b143016b9ba27bcda62c8
Alexandre Gigliotti <alexandre.f.gigliotti@gmail.com>
2017-10-25 12:17:06 -0700

    Add documentation.

--- src/Node/Path.elm
+++ src/Node/Path.elm
@@ -15,25 +15,29 @@ module Node.Path
 import Native.Path
 
 
-{-| -}
+{-| Extract the basename from a filename.
+-}
 basename : String -> String
 basename =
     Native.Path.basename
 
 
-{-| -}
+{-| Extract the directory name from a filename.
+-}
 dirname : String -> String
 dirname =
     Native.Path.dirname
 
 
-{-| -}
+{-| Extract the extension name from a filename.
+-}
 extname : String -> String
 extname =
     Native.Path.extname
 
 
-{-| -}
+{-| Build a path from the list of supplied strings
+-}
 join : List String -> String
 join =
     Native.Path.join
