13e72c4b5b4b5f57ef517baefbc28f019e00d026
Alexandre Gigliotti <alexandre.f.gigliotti@gmail.com>
2017-10-25 13:58:27 -0700

    Add check for NaN in parseInt native code.

--- src/Native/Global.js
+++ src/Native/Global.js
@@ -6,9 +6,9 @@ const _elm_node$core$Native_Global = function () {
     // parseInt : Int -> String -> Result Decode.Value Int
     const parseInt = F2((radix, string) => {
         try {
-            // radix values integer from 2-36
+            // NOTE radix can be any integer from 2-36
             const value = global.parseInt(string, radix)
-            // TODO check for value === NaN and Err in that case
+            if (isNaN(value)) return Err(new Error(`String cannot be converted to an integer: ${string}`))
             return Ok(value)
         } catch (error) { return Err(error) }
     })
